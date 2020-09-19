36a2b0b6d36e7d2327370b32a49398c060ab535d
imranilyas <68575807+imranilyas@users.noreply.github.com>
2020-09-19 01:01:16 -0700

    Rocket Directionality
    Added RigidBody, relative force, rotation, and audio

---
+++ Assets/Rocket.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour
+{
+    Rigidbody rigidBody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProcessInput();
+    }
+
+    private void ProcessInput()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            rigidBody.AddRelativeForce(Vector3.up);
+            print("Spacebar");
+        }
+        if ((Input.GetKey(KeyCode.A) == true) && (Input.GetKey(KeyCode.D) == false))
+        {
+            transform.Rotate(Vector3.forward);
+            print("Rotating Left");
+        }
+        if ((Input.GetKey(KeyCode.D) == true) && (Input.GetKey(KeyCode.A) == false))
+        {
+            transform.Rotate(Vector3.back);
+            print("Rotating Right");
+        }
+    }
+}
