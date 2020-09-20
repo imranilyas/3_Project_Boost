629b7ced3a67f27acd211b1167c6684616876f48
imranilyas <68575807+imranilyas@users.noreply.github.com>
2020-09-20 16:56:43 -0700

    SerializeField levers
    Added thruster and rotation to inspector and made the rocket a little smoother

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 
 public class Rocket : MonoBehaviour
 {
+    [SerializeField] float rcsThrust = 100f;
+    [SerializeField] float mainThrust = 100f;
     Rigidbody rigidBody;
     AudioSource audio;
 
@@ -18,15 +20,15 @@ public class Rocket : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        ProcessInput();
+        ThrusterInput();
+        RotationInput();
     }
 
-    private void ProcessInput()
+    private void ThrusterInput()
     {
-        rigidBody.freezeRotation = true;
         if (Input.GetKey(KeyCode.Space))
         {
-            rigidBody.AddRelativeForce(Vector3.up);
+            rigidBody.AddRelativeForce(Vector3.up * mainThrust);
             print("Spacebar");
             if (!audio.isPlaying)
             {
@@ -37,14 +39,20 @@ public class Rocket : MonoBehaviour
         {
             audio.Stop();
         }
+    }
+    private void RotationInput()
+    {
+        rigidBody.freezeRotation = true;
+        float rotationThisFrame = Time.deltaTime * rcsThrust;
+
         if ((Input.GetKey(KeyCode.A) == true) && (Input.GetKey(KeyCode.D) == false))
         {
-            transform.Rotate(Vector3.forward);
+            transform.Rotate(Vector3.forward * rotationThisFrame);
             print("Rotating Left");
         }
         if ((Input.GetKey(KeyCode.D) == true) && (Input.GetKey(KeyCode.A) == false))
         {
-            transform.Rotate(Vector3.back);
+            transform.Rotate(Vector3.back * rotationThisFrame);
             print("Rotating Right");
         }
         rigidBody.freezeRotation = false;
