20665500a6483c8a7fbebb4e13fe813b7e75c29e
imranilyas <68575807+imranilyas@users.noreply.github.com>
2020-10-03 19:40:40 -0700

    Moving Objects
    updated oscillator script, added time.deltatime to rocket script

--- Assets/Scripts/Oscillator.cs
+++ Assets/Scripts/Oscillator.cs
@@ -5,8 +5,10 @@ using UnityEngine;
 [DisallowMultipleComponent]
 public class Oscillator : MonoBehaviour
 {
-    [SerializeField] Vector3 movementVector;
+    [SerializeField] Vector3 movementVector = new Vector3(10f, 10f, 10f);
     [Range(-1,1)][SerializeField] float movementFactor;
+
+    [SerializeField] float period = 2f;
 
     Vector3 startingPos;
 
@@ -19,6 +21,12 @@ public class Oscillator : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        const float tau = Mathf.PI * 2f; //about 6.28
+        float cycles = Time.time / period;
+        float rawSine = Mathf.Sin(cycles * tau);
+
+        movementFactor = rawSine; //oscillates between -1 and 1
+
         Vector3 offset = movementVector * movementFactor;
         transform.position = startingPos + offset;
     }
