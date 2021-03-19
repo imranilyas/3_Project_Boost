9460d183eafefb7f269cccc3f7195edd877e0347
imranilyas <68575807+imranilyas@users.noreply.github.com>
2021-03-19 11:32:19 -0700

    Robust CameraFollow Script
    edit the camera follow script to follow any combination of directions, new map idea

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -5,10 +5,37 @@ using UnityEngine;
 public class CameraFollow : MonoBehaviour
 {
     public Transform target;
+    enum Axis { x, y, z, xy, xz, yz };
+    [SerializeField] Axis direction;
     public Vector3 offset;
 
     // Update is called once per frame
     void Update()
+    {
+        if (direction == Axis.x) { FollowX(); }
+        if (direction == Axis.y) { FollowY(); }
+        if (direction == Axis.z) { FollowZ(); }
+        if (direction == Axis.xy) { FollowX(); FollowY(); }
+        if (direction == Axis.xz) { FollowX(); FollowZ(); }
+        if (direction == Axis.yz) { FollowY(); FollowZ(); }
+
+    }
+
+    private void FollowX()
+    {
+        Vector3 position = transform.position;
+        position.x = target.position.x + offset.x;
+        transform.position = position;        
+    }
+
+    private void FollowZ()
+    {
+        Vector3 position = transform.position;
+        position.z = target.position.z + offset.z;
+        transform.position = position;
+    }
+
+    private void FollowY()
     {
         Vector3 position = transform.position;
         position.y = target.position.y + offset.y;
